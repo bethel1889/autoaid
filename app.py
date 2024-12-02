d8b60b68877e4fc7c542c8975c01c2a00a01206f
bethel1889 <ibeaghaemmanuel@gmail.com>
2024-12-03 00:54:33 +0100

    Updated directory structure

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///auto_aid.db")
+db = SQL("sqlite:///database/auto_aid.db")
 LOCATIONS = db.execute("SELECT * FROM locations")
 
 @app.after_request
